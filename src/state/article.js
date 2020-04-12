84e90ab94f2d6a9f627f2291bb35746e55bdef82
Mads Harder <mads987a@gmail.com>
2020-04-12 21:30:43 +0200

    Fixed if else statement and formatting

--- src/state/article.js
+++ src/state/article.js
@@ -1,44 +1,44 @@
-import request from './request'
+import request from "./request";
 
 const initialState = {};
 
 export default (state = initialState, action) => {
-    const temp = {...state};
-    switch (action.type) {
-        case "ARTICLES/FETCHED_ARTICLES_METADATA":
-            for(let i = 0; i < action.articles.length; i++){
-                if(temp[action.articles[i].id]){
-                    temp[action.articles[i].id] = {
-                        ...temp[action.articles[i].id],
-                        ...action.articles[i]
-                    }
-                else{
-                    temp[action.articles[i].id] = action.articles[i]
-                }
-            }
-            return temp;
-        case "ARTICLES/FETCHED_ARTICLE":
-            temp[action.article.id] = action.article;
-            return temp;
-        default:
-            return state;
-    }
-}
+	const temp = { ...state };
+	switch (action.type) {
+	case "ARTICLES/FETCHED_ARTICLES_METADATA":
+		for (let i = 0; i < action.articles.length; i++) {
+			if (temp[action.articles[i].id]) {
+				temp[action.articles[i].id] = {
+					...temp[action.articles[i].id],
+					...action.articles[i]
+				};
+			} else {
+				temp[action.articles[i].id] = action.articles[i];
+			}
+		}
+		return temp;
+	case "ARTICLES/FETCHED_ARTICLE":
+		temp[action.article.id] = action.article;
+		return temp;
+	default:
+		return state;
+	}
+};
 
 export const fetchArticleMetadata = (dispatch) => {
-    request("GET", "/articles").then((res) => {
-        dispatch({
-            type : "ARTICLES/FETCHED_ARTICLES_METADATA",
-            articles : res.articles
-        })
-    })
-}
+	request("GET", "/articles").then((res) => {
+		dispatch({
+			type: "ARTICLES/FETCHED_ARTICLES_METADATA",
+			articles: res.articles
+		});
+	});
+};
 
 export const fetchArticle = (dispatch, id) => {
-    request("GET", `/articles/${id}`).then((res) => {
-        dispatch({
-            type : "ARTICLES/FETCHED_ARTICLE",
-            article : res.article
-        })
-    })
-}
+	request("GET", `/articles/${id}`).then((res) => {
+		dispatch({
+			type: "ARTICLES/FETCHED_ARTICLE",
+			article: res.article
+		});
+	});
+};
