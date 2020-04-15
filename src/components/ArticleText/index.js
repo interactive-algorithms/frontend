d2f451d772567a8c40a1c9f39252382348417370
williamBMDK <williambillemeyling@gmail.com>
2020-04-16 01:22:44 +0200

    fix images in sections

--- src/components/ArticleText/index.js
+++ src/components/ArticleText/index.js
@@ -6,19 +6,29 @@ import "./index.css";
 
 export default withTheme(props => {
 
-	const Hstyle = {
-		marginLeft: "-15px",
+	const Hstyle = size => {return {
+		marginLeft: `-${size}px`,
 		paddingLeft: "5px",
-		borderLeft: `15px solid ${props.theme.palette.primary.main}`
-	}
+		borderLeft: `${size}px solid ${props.theme.palette.primary.main}`
+	}}
 
 	const listOfContentElements = props.content.map((item, index) => {
 		if (item.type === "h1") {
-			return <h1 style={Hstyle} key={index}>{item.content}</h1>;
+			return <h1 style={Hstyle(15)} key={index}>{item.content}</h1>;
+		} else if (item.type === "h2") {
+			return <h2 style={Hstyle(13)} key={index}>{item.content}</h2>;
+		} else if (item.type === "h3") {
+			return <h3 style={Hstyle(11)} key={index}>{item.content}</h3>;
+		} else if (item.type === "h4") {
+			return <h4 style={Hstyle(9)} key={index}>{item.content}</h4>;
+		} else if (item.type === "h5") {
+			return <h5 style={Hstyle(7)} key={index}>{item.content}</h5>;
+		} else if (item.type === "h6") {
+			return <h6 style={Hstyle(5)} key={index}>{item.content}</h6>;
 		} else if (item.type === "p") {
 			return <p key={index} >{item.content}</p>;
 		} else if (item.type === "img") {
-			return <img key={index} src={item.content.src} alt={item.content.alt} />;
+			return <img key={index} src={`assets/articleImages/${item.url}`} alt={item.alt} />;
 		} else if (item.type === "ul") {
 			return (
 				<ul key={index}>
