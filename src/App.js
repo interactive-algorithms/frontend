597f4878baab23ebd1745642a85589c2d4be8b59
Mads Harder <mads987a@gmail.com>
2020-05-03 00:25:46 +0200

    Removed some console.log's

--- src/App.js
+++ src/App.js
@@ -12,11 +12,9 @@ import Articles from 'screens/articles'
 import Home from 'screens/home'
 
 import {useSelector} from 'react-redux'
-import { Scrollbars } from 'react-custom-scrollbars';
 
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
-import purple from '@material-ui/core/colors/purple';
 
 const theme = createMuiTheme({
   palette: {
