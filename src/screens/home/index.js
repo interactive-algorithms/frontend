e362cec546faad40228517aa3910d671c3f2f623
Mads Harder <mads987a@gmail.com>
2020-05-02 16:23:46 +0200

    Added a home screen

---
+++ src/screens/home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+
+export default props => {
+	return (
+		<>
+			<Container>
+				<h1 class="display-4">Welcome to Interactive Algorithms</h1>
+				<p class="lead">This is a website create to make it a lot easier to learn </p>
+			</Container>
+		</>
+	);
+}
+
